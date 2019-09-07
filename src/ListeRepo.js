95aa128aa124125a506073626cac930bf8bf62c5
Cedric-Duraysseix <cedric.duraysseix@etu.unilim.fr>
2019-09-07 02:38:53 +0200

    Fin test JobiJoba

---
+++ src/ListeRepo.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import './ListeRepo.css';
+
+class ListeRepo extends Component {
+
+    state = {
+        isLoading: true,
+        repositories: []
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        fetch(`https://api.github.com/search/repositories?q=es6&order=desc`)
+            .then(response => response.json())
+            .then(data =>
+                this.setState({
+                    repositories: data,
+                    isLoading: false
+                })
+            )
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    render() {
+        const { isLoading, repositories } = this.state;
+        console.log(repositories);
+        for(let i=0;i<10;i++){
+            return (
+                <div>
+                    {isLoading ? <p>Loading</p> :
+                        <tbody>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Description</th>
+                            <th>Url</th>
+                            <th>Nombre étoile</th>
+                        </tr>
+                        {repositories && Array.isArray(repositories.items) && repositories.items.map(item => (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.description}</td>
+                                <td>{item.html_url}</td>
+                                <td>{item.stargazers_count}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    }
+                </div>
+            )
+        }
+    }
+}
+
+export default ListeRepo;
